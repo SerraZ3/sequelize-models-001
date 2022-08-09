a703ad08ecc9ca578612218925a41c4fa14ac902
SerraZ3 <serra.henrique3@gmail.com>
2022-08-09 19:41:29 -0300

    feat: criando estrutura do projeto

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+const userController = {
+  index: (req, res) => {},
+  show: (req, res) => {},
+  store: (req, res) => {},
+  update: (req, res) => {},
+  destroy: (req, res) => {},
+};
+module.exports = userController;
